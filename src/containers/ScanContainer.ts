5c41d44d538df87a704322d30643a8527787a4d3
BuiDanhNam-Framgia <bui.danh.nam@framgia.com>
2019-02-28 13:34:11 +0700

    function search

--- src/containers/ScanContainer.ts
+++ src/containers/ScanContainer.ts
@@ -1,12 +1,20 @@
 import {connect} from 'react-redux'
 import ScanScreen from "../screens/ScanScreen";
 import {getInfoUser} from "../redux/actions/InfoUserActions";
+import {getInfoItemScan} from "../redux/actions/ScanQRActions";
 
 const mapStateToProps = (state: any) => {
-    return ({})
+    return ({
+        loading: state.infoItem.loading,
+        data: state.infoItem.data,
+        resultType: state.infoItem.resultType,
+        message: state.infoItem.message,
+        code: state.infoItem.code
+    })
 }
 const mapDispatchToProps = (dispatch: any) => ({
-    getInfoUser : (code: string) => dispatch(getInfoUser(code))
+    getInfoUser: (code: string) => dispatch(getInfoUser(code)),
+    getInfoItem: (code: string) => dispatch(getInfoItemScan(code))
 })
 export default connect(
     mapStateToProps,
--- src/redux/reducers/InfoItemReducer.ts
+++ src/redux/reducers/InfoItemReducer.ts
@@ -3,21 +3,31 @@ import {GetInfoItemType} from '../types'
 const initState = {
     data: '',
     code: '',
-    message: ''
+    message: '',
+    loading: false,
+    resultType: ''
 }
 export default function (state = initState, action: any) {
     switch (action.type) {
         case GetInfoItemType.REQUEST_INFO_ITEM :
             return Object.assign({}, state, {
+                loading: true,
+                resultType: '',
                 code: action.code
             })
         case  GetInfoItemType.REQUEST_INFO_ITEM_SUCCESS :
             return Object.assign({}, state, {
+                loading: false,
+                resultType: "Success",
                 data: action.data,
+                code: action.code
             })
         case GetInfoItemType.REQUEST_INFO_ITEM_FAILED :
             return Object.assign({}, state, {
-                message: action.message
+                loading: false,
+                resultType: "Failure",
+                message: action.message,
+                data:''
             })
         default :
             return state
